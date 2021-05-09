989e0936f97664d13c106bb66881760f56466985
Ruth Pamela <51338723+rpamela05@users.noreply.github.com>
2021-05-09 16:26:20 -0700

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Ruth Carbajal on 5/9/21.
+//  Copyright © 2021 Ruth Carbajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
